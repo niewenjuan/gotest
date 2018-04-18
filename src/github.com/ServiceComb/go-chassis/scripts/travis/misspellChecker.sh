560e30a9e46e7485fb8b12b42f43d076d9f65815
cairixian <cairixian@huawei.com>
2018-04-18 10:33:03 +0800

    xx

--- src/github.com/ServiceComb/go-chassis/scripts/travis/misspellChecker.sh
+++
@@ -1,8 +0,0 @@
-diff -u <(echo -n) <(find . -type f -not -path "./vendor/*" -not -path "./third_party/*" -print0 | xargs -0 misspell)
-if [ $? == 0 ]; then
-	echo "No Misspell found"
-	exit 0
-else
-	echo "Misspell found"
-	exit 1
-fi
